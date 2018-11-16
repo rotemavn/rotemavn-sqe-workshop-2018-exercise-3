68c73fd5001383e01c9ad3a718ab56929a84960b
rotemavn <rotemavn@post.bgu.ac.il>
2018-11-16 17:51:39 +0200

    lint 0 problems

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,6 @@ import $ from 'jquery';
 import {parseCode} from './code-analyzer';
 import {getExpressions, getValues, restartExpressions} from './ast-handler';
 
-debugger;
-
-
-
 var rowCounter = 1;
 let resTable = document.getElementById('resTable');
 
@@ -19,16 +15,6 @@ function createRow(exprObj){
         index++;
         cell.textContent = exprObj[key];
     }
-    // const cell0 = row.insertCell(0);
-    // cell0.textContent = exprObj.line;
-    // const cell1 = row.insertCell(1);
-    // cell1.textContent = exprObj.type;
-    // const cell2 = row.insertCell(2);
-    // cell2.textContent = exprObj.name;
-    // const cell3 = row.insertCell(3);
-    // cell3.textContent = exprObj.condition;
-    // const cell4 = row.insertCell(4);
-    // cell4.textContent = exprObj.value;
 }
 
 
